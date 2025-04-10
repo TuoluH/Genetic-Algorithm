c9ecebf8adecbcb1e1751945bc0c5f9fb00e0bf3
TuoluH <81064670+TuoluH@users.noreply.github.com>
2025-04-10 09:28:17 -0700

    added initial pop and fitness

--- genAlgorithm.js
+++ genAlgorithm.js
@@ -1,26 +1,48 @@
+const width = window.innerWidth
+const height = window.innerHeight
 const phrase = "To be or not to be";
 const popSize = 1000;
 const mutationRate = 0.01;
 let population = [];
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(width, height);
   createInitialPop();
+  textSize(50);
+  textAlign(LEFT, TOP);
+  text("Current Best:", 10, 10);
 }
 
-function draw(){
-  
+function draw() {
+  textSize(12);
+  text(population.join("\n"), width * 0.75, 0);
+  createNewPop(calcFitness());
+
 }
 
 function createInitialPop() {
   for (let i = 0; i < popSize; i++) {
-    population.push(createRandomPhrase());
+    let p = "";
+    for (let i = 0; i < phrase.length; i++) {
+      p += String.fromCharCode(floor(random(32, 126)));
+    }
+    population.push(p);
   }
 }
 
-function createRandomPhrase() {
-  let p = ""
-  for (let i = 0; i < phrase.length; i++) {
-    p += String.fromCharCode(floor(random(32, 126)));
-  }
+function calcFitness() {
+  let matingPool = [];
+  population.forEach( p => {
+    let fitness = 0;
+    for (let l = 0; l < p.length; l++) {
+      p[l] == phrase[l] ? fitness ++ : null;
+    }
+    matingPool.push(...Array(Math.floor(fitness)).fill(p))
+  })
+
+  return matingPool;
+}
+
+function createNewPop() {
+  
 }
